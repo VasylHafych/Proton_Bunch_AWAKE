58477ef61a149b9b7d2230e172fb97fa951708b1
VasylHafych <vasilgafich@gmail.com>
2021-04-05 18:14:31 +0200

    Thesis Updates

---
+++ src/model-41-sym/likelihood.jl
@@ -0,0 +1,296 @@
+function likelihood_cam4(
+        params::NamedTuple, 
+        image::Array{F,2},
+        population::AbstractFloat,
+        cam_ind::Integer;
+        n_threads::Integer = Threads.nthreads()
+    ) where {F <: AbstractFloat}
+    
+    VT = eltype(params.tr_size)
+    tot_loglik::Array{VT} = zeros(VT, n_threads)    
+    light_coefficient::VT = params.cam4_light_amp * 10^5
+    
+    δ_x::VT = params.cam4_psx * 10^-3
+    δ_y::VT = params.cam4_psy * 10^-3
+    
+    @inbounds μ_x::VT  = params.algmx[cam_ind] * δ_x
+    @inbounds μ_y::VT  = params.algmy[cam_ind] * δ_y
+    
+    @inbounds σ_1::VT = sqrt.(params.tr_size[1]^2 + 10^-4*params.ang_spr[1]^2*(params.waist[1] - params.s_cam[cam_ind])^2) 
+    
+    σ_x_1_res::VT = sqrt(σ_1^2 + (params.cam4_resx*δ_x)^2)
+    σ_y_1_res::VT = sqrt(σ_1^2 + (params.cam4_resy*δ_y)^2) 
+    
+    @inbounds σ_2::VT = sqrt.(params.tr_size_2[1]^2 + 10^-4*params.ang_spr_2[1]^2*(params.waist_2[1] - params.s_cam[cam_ind])^2) 
+    
+    σ_x_2_res::VT = sqrt(σ_2^2 + (params.cam4_resx*δ_x)^2)
+    σ_y_2_res::VT = sqrt(σ_2^2 + (params.cam4_resy*δ_y)^2) 
+    
+    dist_1_x = Normal(μ_x, σ_x_1_res)
+    dist_1_y = Normal(μ_y, σ_y_1_res)
+    
+    dist_2_x = Normal(μ_x, σ_x_2_res)
+    dist_2_y = Normal(μ_y, σ_y_2_res)
+    
+    x_edges = range(0, length = size(image)[2]+1, step=δ_x) # should be δ_x
+    y_edges = range(0, length = size(image)[1]+1, step=δ_y) # should be δ_y
+    
+    z1 = diff(cdf.(dist_1_y, y_edges)) * diff(cdf.(dist_1_x, x_edges))' # sigmax and sigmay will be mixed. 
+    z2 = diff(cdf.(dist_2_y, y_edges)) * diff(cdf.(dist_2_x, x_edges))'
+    
+    Threads.@threads for t in eachindex(tot_loglik)
+        
+        cum_log_lik = zero(Float64) 
+        
+        @inbounds for pix_ind in CartesianIndices(image)[t:n_threads:length(image)] 
+            @inbounds if !isnan(image[pix_ind])
+                                
+                pix_prediction = params.mixt_pow*z1[pix_ind] + (1-params.mixt_pow)*z2[pix_ind]
+                pix_prediction = pix_prediction*light_coefficient + params.cam4_ped
+                
+                @inbounds cum_log_lik += logpdf(Normal(pix_prediction, params.cam4_light_fluct*sqrt(pix_prediction)), image[pix_ind]) 
+                
+            end
+        end
+        
+        @inbounds tot_loglik[t] = cum_log_lik
+    end
+    return sum(tot_loglik)
+end
+
+
+function likelihood_cam13(
+        params::NamedTuple, 
+        image::Array{F,2},
+        population::AbstractFloat,
+        cv_matrix::Array{C,2},
+        cv_func::Function, 
+        cam_ind::Integer;
+        n_threads::Integer = Threads.nthreads()
+    ) where {F <: AbstractFloat, C <: AbstractFloat}
+    
+    VT = eltype(params.tr_size)
+    tot_loglik::Array{VT} = zeros(VT, n_threads)    
+    
+    light_coefficient::VT = params.light_amp[cam_ind] * 10^5
+    
+    δ_x::VT = params.psx[cam_ind] * 10^-3
+    δ_y::VT = params.psy[cam_ind] * 10^-3
+    
+    @inbounds μ_x::VT  = params.algmx[cam_ind] * δ_x
+    @inbounds μ_y::VT  = params.algmy[cam_ind] * δ_y
+    
+    @inbounds σ_1::VT = sqrt.(params.tr_size[1]^2 + 10^-4*params.ang_spr[1]^2*(params.waist[1] - params.s_cam[cam_ind])^2) 
+
+    σ_x_1_res::VT = sqrt(σ_1^2 + (params.resx[cam_ind]*δ_x)^2)
+    σ_y_1_res::VT = sqrt(σ_1^2 + (params.resy[cam_ind]*δ_y)^2) 
+    
+    @inbounds σ_2::VT = sqrt.(params.tr_size_2[1]^2 + 10^-4*params.ang_spr_2[1]^2*(params.waist_2[1] - params.s_cam[cam_ind])^2) 
+    
+    σ_x_2_res::VT = sqrt(σ_2^2 + (params.resx[cam_ind]*δ_x)^2)
+    σ_y_2_res::VT = sqrt(σ_2^2 + (params.resy[cam_ind]*δ_y)^2) 
+    
+    dist_1_x = Normal(μ_x, σ_x_1_res)
+    dist_1_y = Normal(μ_y, σ_y_1_res)
+    
+    dist_2_x = Normal(μ_x, σ_x_2_res)
+    dist_2_y = Normal(μ_y, σ_y_2_res)
+    
+    x_edges = range(0, length = size(image)[2]+1, step=δ_x) 
+    y_edges = range(0, length = size(image)[1]+1, step=δ_y) 
+    
+    z1 = diff(cdf.(dist_1_y, y_edges)) * diff(cdf.(dist_1_x, x_edges))'     
+    z2 = diff(cdf.(dist_2_y, y_edges)) * diff(cdf.(dist_2_x, x_edges))'
+    
+    max_pred_amp = size(cv_matrix)[2]-1
+    
+    Threads.@threads for t in eachindex(tot_loglik)
+        
+        cum_log_lik = zero(Float64)
+        
+        @inbounds for pix_ind in CartesianIndices(image)[t:n_threads:length(image)] 
+            @inbounds if !isnan(image[pix_ind])
+                
+                pix_prediction = params.mixt_pow*z1[pix_ind] + (1-params.mixt_pow)*z2[pix_ind]
+                pix_prediction = pix_prediction*light_coefficient
+
+                if pix_prediction > max_pred_amp - 1
+                    cv_vals = - Inf
+                else 
+                    cv_vals = cv_func(cv_matrix, image[pix_ind], pix_prediction)
+                end
+                
+                @inbounds cum_log_lik += cv_vals
+                
+            end
+        end
+        tot_loglik[t] = cum_log_lik
+        
+    end
+
+    return sum(tot_loglik)
+end
+
+function conv_tabl_discrete(cv_matrix::Array{F,2}, observed::Real, expected::Real) where {F<:AbstractFloat}  
+    return cv_matrix[convert(Integer, observed+1), round(Integer, expected+1)]     
+end
+
+"""
+    Generate simulated event for the given parameters (camera 1-3). 
+"""
+function generate_image_cam13(
+        params::T, 
+        population::Float64,
+        cv_matrix::Array{Float64,2},
+        light_fluctuations::Float64,
+        cam_ind::Int64;
+        size::Tuple{Int64, Int64}=(101,101),
+        inc_noise = true,
+        include_satur = true
+    ) where {T <: NamedTuple}
+    
+    image_matrix = zeros(Float64, size...)
+    
+    light_coefficient = params.light_amp[cam_ind] * 10^5
+    
+    δ_x = params.psx[cam_ind] * 10^-3
+    δ_y = params.psy[cam_ind] * 10^-3
+    
+    @inbounds μ_x  = params.algmx[cam_ind] * δ_x
+    @inbounds μ_y  = params.algmy[cam_ind] * δ_y
+    
+    @inbounds σ_1 = sqrt.(params.tr_size[1]^2 + 10^-4*params.ang_spr[1]^2*(params.waist[1] - params.s_cam[cam_ind])^2) 
+    
+    σ_x_1_res = sqrt(σ_1^2 + (params.resx[cam_ind]*δ_x)^2)
+    σ_y_1_res = sqrt(σ_1^2 + (params.resy[cam_ind]*δ_y)^2) 
+    
+    @inbounds σ_2 = sqrt.(params.tr_size_2[1]^2 + 10^-4*params.ang_spr_2[1]^2*(params.waist_2[1] - params.s_cam[cam_ind])^2)  
+    
+    σ_x_2_res = sqrt(σ_2^2 + (params.resx[cam_ind]*δ_x)^2)
+    σ_y_2_res = sqrt(σ_2^2 + (params.resy[cam_ind]*δ_y)^2) 
+    
+    dist_1_x = Normal(μ_x, σ_x_1_res)
+    dist_1_y = Normal(μ_y, σ_y_1_res)
+    
+    dist_2_x = Normal(μ_x, σ_x_2_res)
+    dist_2_y = Normal(μ_y, σ_y_2_res)
+    
+    x_edges = range(0, length = Base.size(image_matrix)[2]+1, step=δ_x)
+    y_edges = range(0, length = Base.size(image_matrix)[1]+1, step=δ_y) 
+    
+    z1 = diff(cdf.(dist_1_y, y_edges)) * diff(cdf.(dist_1_x, x_edges))' 
+    z2 = diff(cdf.(dist_2_y, y_edges)) * diff(cdf.(dist_2_x, x_edges))'
+    
+    bck_cumsum = cumsum(exp.(cv_matrix[:,1]))
+    
+    for pix_ind in CartesianIndices(image_matrix)
+
+        pix_prediction = params.mixt_pow*z1[pix_ind] + (1-params.mixt_pow)*z2[pix_ind]
+        pix_prediction = pix_prediction*light_coefficient
+        
+        if inc_noise
+            
+            pix_prediction = argmin(abs.(cumsum(exp.(cv_matrix[:, round(Int64, pix_prediction)+1])) .- rand())) - 1
+            
+#             pix_prediction = rand(truncated(Normal(pix_prediction, 0.5+light_fluctuations*sqrt(pix_prediction)), 0, 4096))
+#             background_tmp = bck_cumsum .- rand()
+#             background_tmp[background_tmp .< 0 ] .= Inf
+#             pix_prediction += argmin(background_tmp) - 1
+        end
+        
+        if include_satur && pix_prediction > 4095
+            pix_prediction = 4095
+        end
+        
+        image_matrix[pix_ind] = round(Int64, pix_prediction)
+    end
+
+    return image_matrix
+end
+
+"""
+    Generate simulated event for the given parameters (camera 4). 
+"""
+function generate_image_cam4(
+        params::T, 
+        population::Float64,
+        cam_ind::Int64;
+        size::Tuple{Int64, Int64}=(101,101),
+        inc_noise = true,
+        include_satur = true
+    ) where {T <: NamedTuple}
+    
+    image_matrix = zeros(Float64, size...)
+    
+    light_coefficient = params.cam4_light_amp * 10^5
+    
+    δ_x = params.cam4_psx * 10^-3
+    δ_y = params.cam4_psy * 10^-3
+    
+    @inbounds μ_x  = params.algmx[cam_ind] * δ_x
+    @inbounds μ_y  = params.algmy[cam_ind] * δ_y
+    
+    @inbounds σ_1 = sqrt.(params.tr_size[1]^2 + 10^-4*params.ang_spr[1]^2*(params.waist[1] - params.s_cam[cam_ind])^2) 
+    
+    σ_x_1_res = sqrt(σ_1^2 + (params.cam4_resx*δ_x)^2)
+    σ_y_1_res = sqrt(σ_1^2 + (params.cam4_resy*δ_y)^2) 
+    
+    @inbounds σ_2 = sqrt.(params.tr_size_2[1]^2 + 10^-4*params.ang_spr_2[1]^2*(params.waist_2[1] - params.s_cam[cam_ind])^2) 
+    
+    σ_x_2_res = sqrt(σ_2^2 + (params.cam4_resx*δ_x)^2)
+    σ_y_2_res = sqrt(σ_2^2 + (params.cam4_resy*δ_y)^2) 
+    
+#     dist_x = MixtureModel(Normal[Normal(μ_x, σ_x_1_res), Normal(μ_x, σ_x_2_res)], [params.mixt_pow, 1-params.mixt_pow])
+#     dist_y = MixtureModel(Normal[Normal(μ_y, σ_y_1_res), Normal(μ_y, σ_y_2_res)], [params.mixt_pow, 1-params.mixt_pow])
+    
+    dist_1_x = Normal(μ_x, σ_x_1_res)
+    dist_1_y = Normal(μ_y, σ_y_1_res)
+    
+    dist_2_x = Normal(μ_x, σ_x_2_res)
+    dist_2_y = Normal(μ_y, σ_y_2_res)
+    
+    x_edges = range(0, length = Base.size(image_matrix)[2]+1, step=δ_x) 
+    y_edges = range(0, length = Base.size(image_matrix)[1]+1, step=δ_y) 
+    
+    z1 = diff(cdf.(dist_1_y, y_edges)) * diff(cdf.(dist_1_x, x_edges))' 
+    z2 = diff(cdf.(dist_2_y, y_edges)) * diff(cdf.(dist_2_x, x_edges))'
+    
+    for pix_ind in CartesianIndices(image_matrix)
+        
+        pix_prediction = params.mixt_pow*z1[pix_ind] + (1-params.mixt_pow)*z2[pix_ind]
+        pix_prediction = pix_prediction*light_coefficient + params.cam4_ped
+        
+        if inc_noise
+            pix_prediction = rand(Normal(pix_prediction, params.cam4_light_fluct*sqrt(pix_prediction))) 
+        end
+        
+        if include_satur && pix_prediction > 4095
+            pix_prediction = 4095
+        end
+        
+        image_matrix[pix_ind] = round(Int64, pix_prediction)
+    end
+
+    return image_matrix
+end
+
+
+"""
+    Generate simulated event using 4 cameras. 
+"""
+function generate_event(
+        params::D, population::Float64, conv_mat::T; 
+        inc_noise=true,
+        size = [(70, 70),(70, 70),(40, 40),(70, 70)],
+        light_fluctuations = 2.0,
+        include_satur = true,
+    ) where {T<: NamedTuple, D <: NamedTuple}
+
+    
+    img_1 = generate_image_cam13(params, population, conv_mat.cam_1, light_fluctuations, 1, size = size[1], inc_noise=inc_noise, include_satur=include_satur)
+    img_2 = generate_image_cam13(params, population, conv_mat.cam_2, light_fluctuations, 2, size = size[2], inc_noise=inc_noise, include_satur=include_satur)
+    img_3 = generate_image_cam13(params, population, conv_mat.cam_3, light_fluctuations, 3, size = size[3], inc_noise=inc_noise, include_satur=include_satur)
+    img_4 = generate_image_cam4(params, population, 4, size = size[4], inc_noise=inc_noise, include_satur=include_satur)
+    
+    return (cam_1 = img_1, cam_2 = img_2, cam_3 = img_3, cam_4 = img_4, population = population)
+end
---
+++ src/model-41-sym/utill_model.jl
@@ -0,0 +1,237 @@
+"""
+    Generate envelope trajectories for multiple parameters. No resolution effects included. The first trajectory will be filled with a gray color and can be considered as a "truth" trajectory.  
+"""
+function plot_envelop_trajectory(
+        params_array; 
+        colors = ["gray", "red", "C3", "C4", "C5", "C6", "C7", "C8", "C9"],
+        labels = [i for i in 1:9],
+        figsize=(8,6)
+        ) 
+    
+    fig, ax = plt.subplots(2,1, figsize=figsize, sharex=true)
+    fig.subplots_adjust(hspace=0.0, wspace=0.0)
+    
+    x_range = range(-1, stop = 24, length=100)
+    
+    for (ind, params) in enumerate(params_array)
+        
+        σ_x(x) = sqrt.(params.tr_size[1]^2 + 10^-4*params.ang_spr[1]^2*(params.waist[1] - x)^2) 
+        σ_x_2(x) = sqrt.(params.tr_size_2[1]^2 + 10^-4*params.ang_spr_2[1]^2*(params.waist_2[1] - x)^2) 
+    
+        σ_x_vals = [σ_x(x) for x in x_range]
+        σ_y_vals = [σ_x(x) for x in x_range]
+        
+        σ_x_vals_2 = [σ_x_2(x) for x in x_range]
+        σ_y_vals_2 = [σ_x_2(x) for x in x_range]
+
+        ax[1].set_ylim(0, maximum(σ_x_vals))
+        ax[2].set_ylim(0, maximum(σ_y_vals))
+        
+        if ind == 1 
+            
+#             ax[1].vlines(params.s_cam, 0,  maximum(σ_x_vals), linestyle="-", color="darkslategray", alpha=0.5)
+#             ax[2].vlines(params.s_cam, 0,  maximum(σ_y_vals), linestyle="-", color="darkslategray", alpha=0.5)
+            
+            ax[1].axvline(params.s_cam[1], linestyle="-", color="darkslategray", alpha=0.5)
+            ax[1].axvline(params.s_cam[2], linestyle="-", color="darkslategray", alpha=0.5)
+            ax[1].axvline(params.s_cam[3], linestyle="-", color="darkslategray", alpha=0.5)
+            ax[1].axvline(params.s_cam[4], linestyle="-", color="darkslategray", alpha=0.5)
+            
+            ax[2].axvline(params.s_cam[1], linestyle="-", color="darkslategray", alpha=0.5)
+            ax[2].axvline(params.s_cam[2], linestyle="-", color="darkslategray", alpha=0.5)
+            ax[2].axvline(params.s_cam[3], linestyle="-", color="darkslategray", alpha=0.5)
+            ax[2].axvline(params.s_cam[4], linestyle="-", color="darkslategray", alpha=0.5)
+
+            ax[1].plot(x_range, σ_x_vals, color=colors[ind], ls="--", label="Halo"*labels_array[ind])
+            ax[2].plot(x_range, σ_y_vals, color=colors[ind], ls="--",)
+            
+            ax[1].plot(x_range, σ_x_vals_2, color=colors[ind], ls=":", label="Core"*labels_array[ind])
+            ax[2].plot(x_range, σ_y_vals_2, color=colors[ind], ls=":",)
+            
+            ax[1].set_xlim(-1, maximum(x_range))
+
+            ax[1].set_ylim(0, maximum(σ_x_vals))
+            ax[2].set_ylim(0, maximum(σ_y_vals))
+        else 
+            
+            ax[1].plot(x_range, σ_x_vals, color=colors[ind], ls="--", label="Halo"*labels_array[ind])
+            ax[2].plot(x_range, σ_y_vals, color=colors[ind], ls="--",)
+            
+            ax[1].plot(x_range, σ_x_vals_2, color=colors[ind], ls=":", label="Core"*labels_array[ind])
+            ax[2].plot(x_range, σ_y_vals_2, color=colors[ind], ls=":",)
+        end
+    end
+
+    ax[1].legend(loc="upper left", ncol=5, framealpha=1)
+    ax[1].set_ylabel(L"\sigma_x, [mm]")
+    ax[2].set_ylabel(L"\sigma_y, [mm]")
+    ax[2].set_xlabel(L"s, [m]")
+end
+
+function plot_signal_envelope(params)
+    
+    color_1 = "sandybrown"
+    color_2 = "lightsteelblue"
+    
+    size = (10, 8)
+    
+    fig = plt.figure(figsize=size,)
+
+    fig.subplots_adjust(hspace=0.00, wspace=0.00)
+
+    r11 = plt.subplot2grid((13, 4), (0, 0), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    r12 = plt.subplot2grid((13, 4), (0, 1), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    r13 = plt.subplot2grid((13, 4), (0, 2), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    r14 = plt.subplot2grid((13, 4), (0, 3), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    rst1 = [r11, r12, r13, r14]
+
+    r2 = plt.subplot2grid((13, 4), (3, 0), rowspan=3, colspan=4, )
+    r2.tick_params(axis="x", pad=8)
+    r3 = plt.subplot2grid((13, 4), (7, 0), rowspan=3, colspan=4, xticklabels=[], )
+
+    r3.xaxis.tick_top()
+
+    r41 = plt.subplot2grid((13, 4), (10, 0), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    r42 = plt.subplot2grid((13, 4), (10, 1), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    r43 = plt.subplot2grid((13, 4), (10, 2), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    r44 = plt.subplot2grid((13, 4), (10, 3), rowspan=3, colspan=1, yticks=[],  xticks=[], )
+    rst2 = [r41, r42, r43, r44]
+    
+    # make plot
+    
+    x_range = range(-1, stop = 24, length=100)
+    σ_x(x) = sqrt.(params.tr_size[1]^2 + 10^-4*params.ang_spr[1]^2*(params.waist[1] - x)^2) 
+    σ_x_2(x) = sqrt.(params.tr_size_2[1]^2 + 10^-4*params.ang_spr_2[1]^2*(params.waist_2[1] - x)^2) 
+
+    σ_y(x) = sqrt.(params.tr_size[2]^2 + 10^-4*params.ang_spr[2]^2*(params.waist[1] - x)^2) 
+    σ_y_2(x) = sqrt.(params.tr_size_2[2]^2 + 10^-4*params.ang_spr_2[2]^2*(params.waist_2[1] - x)^2) 
+
+    σ_x_vals = [σ_x(x) for x in x_range]
+    σ_y_vals = [σ_y(x) for x in x_range]
+
+    σ_x_vals_2 = [σ_x_2(x) for x in x_range]
+    σ_y_vals_2 = [σ_y_2(x) for x in x_range]
+    
+    r2.fill_between(x_range, σ_x_vals, color=color_1, alpha=0.5, )
+    r3.fill_between(x_range, σ_y_vals, color=color_1, alpha=0.5, label="Halo Component")
+            
+    r2.fill_between(x_range, σ_x_vals_2, color=color_2, alpha=0.5, )
+    r3.fill_between(x_range, σ_y_vals_2, color=color_2, alpha=0.5, label="Core Component")
+    r3.legend(bbox_to_anchor=(0.2, 0.01, 0.4, 0.15), ncol=2, framealpha=0.0)
+    
+    r2.axvline(params.s_cam[1], linestyle=":", color="darkslategray", alpha=0.5, label="Cam. Position")
+    r2.axvline(params.s_cam[2], linestyle=":", color="darkslategray", alpha=0.5)
+    r2.axvline(params.s_cam[3], linestyle=":", color="darkslategray", alpha=0.5)
+    r2.axvline(params.s_cam[4], linestyle=":", color="darkslategray", alpha=0.5)
+            
+    r3.axvline(params.s_cam[1], linestyle=":", color="darkslategray", alpha=0.5)
+    r3.axvline(params.s_cam[2], linestyle=":", color="darkslategray", alpha=0.5)
+    r3.axvline(params.s_cam[3], linestyle=":", color="darkslategray", alpha=0.5)
+    r3.axvline(params.s_cam[4], linestyle=":", color="darkslategray", alpha=0.5)
+    
+    r2.axvline(params.waist[1], linestyle="-", color=color_1, alpha=1, label="Waist Halo")
+    r3.axvline(params.waist[1], linestyle="-", color=color_1, alpha=1)
+    r2.axvline(params.waist_2[1], linestyle="-", color=color_2, alpha=1, label="Waist Core")
+    r3.axvline(params.waist_2[1], linestyle="-", color=color_2, alpha=1)
+    
+    r2.axvline(2.9, linestyle=":", color="red", alpha=1, label="Waist Expected")
+    r3.axvline(2.9, linestyle=":", color="red", alpha=1, label="Expected")
+    
+    r2.legend(bbox_to_anchor=(0.6, 0.98,), ncol=2, framealpha=0.0)
+    
+    for i in 1:4
+        if i < 4
+            npix = i < 3 ? 71 : 41
+            x_bins = range(0, length = npix+1, step = params.psx[i]*10^-3)
+            y_bins = range(0, length = npix+1, step = params.psy[i]*10^-3)
+                
+            mue_x = maximum(x_bins) / 2
+            mue_y = maximum(y_bins) / 2
+            
+            σ_x_vls = σ_x(params.s_cam[i])
+            σ_x_2_vls = σ_x_2(params.s_cam[i])
+            
+            σ_y_vls = σ_y(params.s_cam[i])
+            σ_y_2_vls = σ_y_2(params.s_cam[i])
+    
+            vals_x_1 = diff(cdf.(Normal(mue_x, σ_x_vls), x_bins))
+            vals_x_2 = diff(cdf.(Normal(mue_x, σ_x_2_vls), x_bins))
+
+            vals_y_1 = diff(cdf.(Normal(mue_y, σ_y_vls), y_bins))
+            vals_y_2 = diff(cdf.(Normal(mue_y, σ_y_2_vls), y_bins))
+            
+            rst1[i].fill_between(x_bins[1:end-1], params.mixt_pow .* vals_x_1, color=color_1, alpha=0.5, label="Halo")
+            rst1[i].fill_between(x_bins[1:end-1], (1-params.mixt_pow).*vals_x_2, color=color_2, alpha=0.5, label="Core")
+            rst1[i].plot(x_bins[1:end-1], (1-params.mixt_pow).*vals_x_2 .+ params.mixt_pow .* vals_x_1, color="k", ls="--", label="Sum")
+            
+            rst2[i].fill_between(y_bins[1:end-1], params.mixt_pow .* vals_y_1, alpha=0.5, color=color_1, )
+            rst2[i].fill_between(y_bins[1:end-1], (1-params.mixt_pow).*vals_y_2, alpha=0.5, color=color_2, )
+            rst2[i].plot(y_bins[1:end-1], (1-params.mixt_pow).*vals_y_2 .+ params.mixt_pow .* vals_y_1, color="k", ls="--")
+            
+        else
+            npix = 71
+            x_bins = range(0, length = npix+1, step = params.cam4_psx*10^-3)
+            y_bins = range(0, length = npix+1, step = params.cam4_psy*10^-3)
+            
+            mue_x = maximum(x_bins) / 2
+            mue_y = maximum(y_bins) / 2
+            
+            σ_x_vls = σ_x(params.s_cam[i])
+            σ_x_2_vls = σ_x_2(params.s_cam[i])
+            
+            σ_y_vls = σ_y(params.s_cam[i])
+            σ_y_2_vls = σ_y_2(params.s_cam[i])
+    
+            vals_x_1 = diff(cdf.(Normal(mue_x, σ_x_vls), x_bins))
+            vals_x_2 = diff(cdf.(Normal(mue_x, σ_x_2_vls), x_bins))
+
+            vals_y_1 = diff(cdf.(Normal(mue_y, σ_y_vls), y_bins))
+            vals_y_2 = diff(cdf.(Normal(mue_y, σ_y_2_vls), y_bins))
+            
+            rst1[i].fill_between(x_bins[1:end-1], params.mixt_pow .* vals_x_1, color=color_1, alpha=0.5, label="Halo")
+            rst1[i].fill_between(x_bins[1:end-1], (1-params.mixt_pow).*vals_x_2, color=color_2, alpha=0.5, label="Core")
+            rst1[i].plot(x_bins[1:end-1], (1-params.mixt_pow).*vals_x_2 .+ params.mixt_pow .* vals_x_1, color="k",  ls="--", label="Sum")
+            
+            rst2[i].fill_between(y_bins[1:end-1], params.mixt_pow .* vals_y_1, color=color_1, alpha=0.5, )
+            rst2[i].fill_between(y_bins[1:end-1], (1-params.mixt_pow).*vals_y_2, color=color_2, alpha=0.5, )
+            rst2[i].plot(y_bins[1:end-1], (1-params.mixt_pow).*vals_y_2 .+ params.mixt_pow .* vals_y_1, color="k", ls="--")
+            
+        end
+    end
+    
+    r2.text(0.015,0.62,"Cam. 1", rotation="vertical", color="darkslategray", transform=r2.transAxes) 
+    r2.text(0.075,0.62,"Cam. 2", rotation="vertical", color="darkslategray", transform=r2.transAxes) 
+    r2.text(0.62,0.62,"Cam. 3", rotation="vertical", color="darkslategray", transform=r2.transAxes) 
+    r2.text(0.94,0.62,"Cam. 4", rotation="vertical", color="darkslategray", transform=r2.transAxes) 
+    
+    r11.text(0.04,0.89,"Cam. 1", color="darkslategray", transform=r11.transAxes)
+    r12.text(0.04,0.89,"Cam. 2", color="darkslategray", transform=r12.transAxes)
+    r13.text(0.04,0.89,"Cam. 3", color="darkslategray", transform=r13.transAxes)
+    r14.text(0.04,0.89,"Cam. 4", color="darkslategray", transform=r14.transAxes)
+
+    fig.text(0.92, 0.25, "Projection Y", rotation="vertical")
+    fig.text(0.92, 0.65, "Projection X", rotation="vertical")
+    
+    r2.set_ylabel(L"\sigma_x \;(mm)")
+    r3.set_ylabel(L"\sigma_y \;(mm)")
+    
+    r2.set_xlim(-1, maximum(x_range))
+    r3.set_xlim(-1, maximum(x_range))
+    
+    max_sigma = maximum([maximum(σ_x_vals), maximum(σ_y_vals), maximum(σ_x_vals_2), maximum(σ_y_vals_2)])
+    
+    r2.set_ylim(bottom=0.0, top = max_sigma)
+    r3.set_ylim(bottom=0.0, top = max_sigma)
+    r3.invert_yaxis()
+
+    r11.set_ylim(bottom=0.0)
+    r12.set_ylim(bottom=0.0)
+    r13.set_ylim(bottom=0.0)
+    r14.set_ylim(bottom=0.0)
+
+    r41.set_ylim(bottom=0.0)
+    r42.set_ylim(bottom=0.0)
+    r43.set_ylim(bottom=0.0)
+    r44.set_ylim(bottom=0.0)
+     
+end
